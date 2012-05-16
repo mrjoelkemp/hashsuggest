b8b5edf5a3d01369237255dd408d08fe90a5a457
Wai Khoo <wlkhoo@gmail.com>
2012-05-16 16:35:10 -0400

    partial definition of cluster

--- cluster.py
+++ cluster.py
@@ -1,4 +1,17 @@
-# Author: Wai
+# Author: Wai Khoo
+
 class Cluster:
-	# TODO: Implement attributes
-	
+	# Init the object
+	def __init__(self, data):
+		self.tweets = data	# store a list of tweets
+		self.centroid = ''	# identify one of the tweets as centroid
+		self.tf = []		# term frequency
+		self.dt = ''		# dominant term
+		
+	# Printing out the data
+	def __repr__(self):
+		print 'Cluster %s: %s' % (self.dt, len(self.tweets))
+		for t in self.tweets:
+			print t
+			
+		return ''
