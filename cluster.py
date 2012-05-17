266cd997b5c930082d6b14e4675f4d5315e58c35
Wai Khoo <wlkhoo@gmail.com>
2012-05-17 17:00:20 -0400

    Implemented class Cluster with TF and DT

--- cluster.py
+++ cluster.py
@@ -1,5 +1,7 @@
 # Author: Wai Khoo
 
+from sets import Set
+
 class Cluster:
 	# Init the object
 	def __init__(self, data):
@@ -15,3 +17,51 @@ class Cluster:
 			print t
 			
 		return ''
+		
+	# Retrieval functions for data members
+	def getTweets(self):
+		return self.tweets
+		
+	def getCentroid(self):
+		return self.centroid
+		
+	def getTF(self):
+		return self.tf
+		
+	def getDT(self):
+		return self.dt
+		
+	# Set centroid (c is a string)
+	def setCentroid(self, c):
+		self.centroid = c
+		
+	# Compute term frequency from tweets and extract dominant term
+	def computeTF_DT(self):
+		str_tweets = self.tokenise(self.joinString(self.tweets))
+		unique_tweets = Set((word for word in str_tweets))
+		
+		self.tf = dict([(term, self.freq(term, str_tweets)) for term in unique_tweets])
+		
+		result = sorted(self.tf.items(), key=lambda x:x[1], reverse=True)
+		self.dt = result[0][0]
+		
+	# Helper function to compute frequency
+	def freq(self, word, sentence):
+		return sentence.count(word)
+		
+	# break string up into tokens
+	def tokenise(self, string):
+		""" break string up into tokens """
+		string = string.replace(".", "")
+		string = string.replace(",", "")
+		string = string.replace("\s+", " ")
+		string = string.lower()
+		words = string.split(" ")
+		return [word for word in words]
+	
+	# Convert a list into a single string
+	def joinString(self, list):
+		str_list = []
+		for tweet in list:
+			str_list.append(tweet + ' ')
+		return ''.join(str_list)
--- mainwai.py
+++ mainwai.py
@@ -1,10 +1,10 @@
 import cluster
 
 def main():
-	data = ["This is tweet1", "Another tweet 2", "yet another tweet 3", "omg, so many tweets 4"]
+	data = ["This is is is tweet1", "Another tweet2 again again and again", "yet so another tweet3, tweet3 again", "omg, so many tweets4"]
 	
 	c = cluster.Cluster(data)
-	
-	print c
+	c.computeTF_DT()
+#	print c
 	
 main()
