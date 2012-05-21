2f2846b0aa71d362c9fd84c68f1890f36f21612f
Joel Kemp <mrjoelkemp@gmail.com>
2012-05-21 15:37:59 -0400

    Moved preprocessing of ashton's tweets to a separate file to make room for other code.

---
+++ data/preprocessashton.py
@@ -0,0 +1,43 @@
+# Author: 	Joel Kemp
+# Purpose: 	Script to preprocess tweets and store them in a file for later consumption.
+
+from preprocess import *
+def main():
+	try:
+		# Open the tweet file
+		file = open("tweetsashton.txt")
+		
+		# Grab all of the tweets from file.
+		tweets = []
+		for tweet in file:
+			tweet = tweet.lower()
+			# split the tweet into tokens
+			tokens = tweet.split()
+			tweets.append(tokens)
+
+		file.close()
+
+		file_write = open("tweetsprocessedashton.txt", "w")
+		
+		for tokens in tweets:
+			# Remove stop words from the list of tokens
+			stopped = removeStopWords(tokens)
+			
+			# Remove the stems
+			stems = removeStems(stopped)
+			
+			# Get a string representation of the list
+			tweet_string = get_tweet_string(stems)
+			
+			# Prevent blanks from being stored
+			if tweet_string == "":
+				continue 
+			
+			# Store the processed tweets in a new file
+			file_write.write(tweet_string + "\n")
+
+		file_write.close()
+		print "Done creating preprocessed tweet file."
+
+	except Exception, e:
+		print "Error:", e
