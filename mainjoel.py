1a1f288a658b600403232643e6c718dc0bf772b0
Joel Kemp <mrjoelkemp@gmail.com>
2012-05-21 17:37:07 -0400

    Changed to int division to get floor of a third.

--- mainjoel.py
+++ mainjoel.py
@@ -1,24 +1,33 @@
 # Author: 	Joel Kemp
 # Purpose: 	Driver program
 from preprocess import *
-
+import segmentation
+import random
 
 def main():
 	processed_tweets = "data/tweetsprocessedashton.txt"
-
 	raw_tweets = "data/tweetsashton.txt"
 	
 	# Grab a stem -> word mapping from the file
 	LUT = get_LUT(raw_tweets)
 
-	#for stem, word in LUT.items():
-	#	print stem, "->", word
+	# Open the tweet file
+	file = open(processed_tweets)
+	tweets = []
+	for t in file:
+		tweets.append(t.replace("\n", ""))
 
-	print "Num Pairings: ", len(list(LUT.keys()))
+	print "Num tweets: ", len(tweets)
 
-	# uniques = get_unique_words(raw_tweets)
-	# for unique in uniques:
-	# 	print unique
+	# Training set
+	subtweets = random.sample(tweets, len(tweets) // 3)
+	print "Num training: ", len(subtweets)
+
+	# Testing set
+	testing = list(set(tweets) - set(subtweets))
+	print "Num testing: ", len(testing)
+
+	#clusters = segmentation.kmeans(subtweets, 5, 20, 0.8)
 
 
 def rem():
