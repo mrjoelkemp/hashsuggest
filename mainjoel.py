851dee2cdc8e0df7d884042f1f5ebb47572b3b46
Joel Kemp <mrjoelkemp@gmail.com>
2012-05-21 18:39:32 -0400

    Query processing moved to preprocess for modularity.

--- mainjoel.py
+++ mainjoel.py
@@ -15,8 +15,9 @@ def main():
 	file = open(processed_tweets)
 	tweets = []
 	
-
+	# Grab all of the tweets
 	for t in file:
+		t = t.lower()
 		tweet = t.replace("\n", "")
 		# Avoid duplicates
 		if tweet in tweets:
@@ -28,25 +29,32 @@ def main():
 
 	# Training set
 	num_training = len(tweets) // 3
-	print "Num Training Ideal: ", num_training
+	#print "Num Training Ideal: ", num_training
 	subtweets = random.sample(tweets, num_training)
-	print "Num training: ", len(subtweets)
+	#print "Num training: ", len(subtweets)
 
 	# Testing set
 	#testing = diff(tweets, subtweets)
 	testing = [tweet for tweet in tweets if tweet not in subtweets]
-	print "Num testing: ", len(testing)
-	print "Sets diff: ", len(tweets) - (len(subtweets) + len(testing))
+	#print "Num testing: ", len(testing)
+	#print "Sets diff: ", len(tweets) - (len(subtweets) + len(testing))
 
+	# Perform k-means on the subtweets
+	clusters = segmentation.kmeans(subtweets, 5, 20, 0.8)
 
-	#clusters = segmentation.kmeans(subtweets, 5, 20, 0.8)
+	## DEBUG: Sample querying
+	query = "I want to save the world by rescuing Uganda and all of its children with my ex-wife Demi."
+	
+	query_tokens = process_query(tweet)
 
+	
 def process_query(tweet):
 	"""
 	Purpose: 	Processes the passed in user-submitted tweet.
 	Precond: 	tweet is a user-submitted string
 	Returns: 	A preprocessed list of words from the passed tweet.
 	"""
+	tweet = tweet.lower()
 	# Break the tweet into a list
 	tweet_words = tweet.split()
 
